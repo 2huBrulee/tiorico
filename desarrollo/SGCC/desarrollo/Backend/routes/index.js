bc6083e3f8051324df313e980cedf9d65e330be4
rimegaray <31824019+rimegaray@users.noreply.github.com>
2019-07-02 13:11:41 -0500

    Update index.js

---
+++ desarrollo/SGCC/desarrollo/Backend/routes/index.js
@@ -0,0 +1,31 @@
+
+    /*
+        Post transaction
+    */
+    .post( url.urlV1 + '/transactions',[
+        check('idUser').exists().isInt().withMessage('Debe ser un número entero.'),
+        check('clientName').exists().isString(),
+        check('clientAddress').exists().isString(),
+        check('clientDocumentNumber').exists().isInt().withMessage('Debe ser un número entero.'),
+        checkSchema({
+            clientDocumentType:{
+                exists: true,
+                isString: true,
+                isIn: {
+                    options: [['DNI']],
+                    errorMessage: 'Opciones válidas: DNI'
+                },
+                errorMessage: 'No válido'
+            },
+            transactionType:{
+                exists:true,
+                isString:true,
+                isIn: {
+                    options: [['COMPRAR','VENDER']],
+                    errorMessage: 'Opciones válidas: COMPRAR,VENDER'
+                },
+                errorMessage: 'No válido'
+            }
+        }),
+        check('quantity').exists().isFloat(),
+    ], TioRicoController.save)
